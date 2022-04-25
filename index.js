44b127692dfc7115a20be9002cc7df60c0bea45d
Bruce <brucealberto2@gmail.com>
2022-04-25 16:15:43 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 // const fs = require('fs/promises');
+const crypto = require('crypto');
 const talker = require('./talker.json');
 
 const app = express();
@@ -15,7 +16,7 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', (req, res) => 
-  // if (!talker) res.status(200).send([]);
+  //  if (!talker) res.status(200).send([]);
    res.status(200).json(talker));
 
 // app.get('/talker', async (req, res) => {
@@ -32,6 +33,13 @@ app.get('/talker/:id', (req, res) => {
       .json({ message: 'Pessoa palestrante não encontrada' }); 
 }
   return res.status(200).json(findId);
+});
+
+app.post('/login', (req, res) => {
+  // const { token } = req.headers;
+  // const { email, password } = req.body;
+  const generateToken = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token: generateToken });
 });
 
 app.listen(PORT, () => {
